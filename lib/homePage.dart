b1810f275041c0659052adc7e1c5f33842d0882c
swathieventhq <swathi.chandraiah@eventhq.io>
2023-01-05 11:41:33 +0530

    first commit

---
+++ lib/homePage.dart
@@ -0,0 +1,30 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'signUpWidget.dart';
+import 'loggedInWidget.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (snapshot.hasData) {
+            return LoggedInWidget();
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Something Went Wrong'));
+          } else {
+            return SignUpWidget();
+          }
+        },
+      ),
+    );
+  }
+}
